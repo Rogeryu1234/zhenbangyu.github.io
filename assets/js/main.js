520fcde6bbee5fb7f6d86bf2ee257f783fb7cb8d
Roger Yu <roger_yu@berkeley.edu>
2024-05-04 19:07:23 -0700

    project 4 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 */
 
 
-// Show more, show less
+// initially display nothing.
 document.addEventListener("DOMContentLoaded", function() {
     // Get all project titles
     var projectTitles = document.querySelectorAll(".project-title");
@@ -30,6 +30,31 @@ document.addEventListener("DOMContentLoaded", function() {
         title.querySelector(".expand-icon").textContent = "Show More";
     });
 });
+
+// initially display everything.
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Get all project titles
+//     var projectTitles = document.querySelectorAll(".project-title");
+
+//     // Add click event listener to each project title
+//     projectTitles.forEach(function(title) {
+//         title.addEventListener("click", function() {
+//             // Toggle the visibility of the project description
+//             var description = this.nextElementSibling;
+//             if (description.style.display === "none") {
+//                 description.style.display = "block";
+//                 this.querySelector(".expand-icon").textContent = "Show Less";
+//             } else {
+//                 description.style.display = "none";
+//                 this.querySelector(".expand-icon").textContent = "Show More";
+//             }
+//         });
+
+//         // Ensure the description is initially displayed
+//         title.nextElementSibling.style.display = "block";
+//     });
+// });
+
 
 
 (function($) {
